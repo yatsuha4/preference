e896b2e1ea3fdbb585bc64659dec744a80afdb7a
Yasui Kentarow <yasuikentarow@gmail.com>
2022-07-16 17:36:36 +0900

    Windows11+WSL2環境整備中

---
+++ emacs.el
@@ -0,0 +1,29 @@
+;;
+(defun insert-colon-space ()
+  (interactive)
+  (insert ", ")
+  )
+
+;; C#
+(defun my-csharp-mode-hook ()
+  (local-set-key "," 'insert-colon-space)
+  (local-set-key "\M-j" 'indent-region)
+  (setq c-basic-offset 4)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4)
+  )
+(add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
+
+;;
+(global-font-lock-mode 0)
+(global-set-key "\M-g" 'goto-line)
+(global-set-key "\M-s" 'sort-lines)
+(prefer-coding-system 'utf-8)
+(setq electric-indent-chars (remq ?\n electric-indent-chars))
+(setq frame-title-format "%b")
+(setq inhibit-startup-message t)
+(setq initial-frame-alist '((width . 100) (height . 50)))
+(setq-default auto-save-default nil)
+(setq-default kill-whole-line t)
+(setq-default make-backup-files nil)
+(setq visible-bell t)
